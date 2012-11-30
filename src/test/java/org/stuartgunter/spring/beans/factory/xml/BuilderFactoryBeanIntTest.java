6c047462bd6e2b2dbf686a944b6d471e85d9e831
Stuart Gunter <Stuart.Gunter@betfair.com>
2012-11-30 12:44:47 +0000

    Added test case for building Guava's ImmutableList

--- src/test/java/org/stuartgunter/spring/beans/factory/xml/BuilderFactoryBeanIntTest.java
+++ src/test/java/org/stuartgunter/spring/beans/factory/xml/BuilderFactoryBeanIntTest.java
@@ -1,11 +1,17 @@
 package org.stuartgunter.spring.beans.factory.xml;
 
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Iterables;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 import org.testng.annotations.Test;
 
+import java.util.List;
+
 import static org.testng.AssertJUnit.assertEquals;
 import static org.testng.AssertJUnit.assertNotNull;
+import static org.testng.AssertJUnit.assertTrue;
+import static org.testng.internal.junit.ArrayAsserts.assertArrayEquals;
 
 @Test
 @ContextConfiguration("classpath:/test-app-context.xml")
@@ -30,5 +36,13 @@ public class BuilderFactoryBeanIntTest extends AbstractTestNGSpringContextTests 
 
         assertNotNull(bean);
         assertEquals("Hello World!", bean);
+    }
+
+    public void shouldGetImmutableListBean() {
+        final List<String> bean = (List<String>) applicationContext.getBean("immutableListBean");
+
+        assertNotNull(bean);
+        assertTrue(bean instanceof ImmutableList);
+        assertArrayEquals(new String[] {"Hello", " World", "!"}, bean.toArray());
     }
 }
